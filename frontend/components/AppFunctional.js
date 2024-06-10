1f1528e98a142d77913b2a1c370535c736229019
Christopher Ralph <chris.734c@gmail.com>
2024-06-09 21:33:32 -0400

    Fixed the getNextIndex logic, changed col + row in getXYMessage to x + y, and implemented post on submit.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -22,17 +22,17 @@ export default function AppFunctional(props) {
   function getXY(idx) {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-    const row = Math.floor(idx / 3) + 1;
-    const col = (idx % 3) + 1;
-    return { row, col };
+    const x = (idx % 3) + 1;
+    const y = Math.floor(idx / 3) + 1;
+    return { x, y };
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-    const { row, col } = getXY(userIndex);
-    return `Coordinates (${col}, ${row})`;
+    const { x, y } = getXY(userIndex);
+    return `Coordinates (${x}, ${y})`;
   }
 
   function reset() {
@@ -47,12 +47,39 @@ export default function AppFunctional(props) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    let nextIndex = userIndex;
+
+    if (direction === "left") {
+      if (userIndex % 3 !== 0) {
+        nextIndex = userIndex - 1;
+      }
+    } else if (direction === "up") {
+      if (userIndex >= 3) {
+        nextIndex = userIndex - 3;
+      }
+    } else if (direction === "right") {
+      if ((userIndex + 1) % 3 !== 0) {
+        nextIndex = userIndex + 1;
+      }
+    } else if (direction === "down") {
+      if (userIndex < 6) {
+        nextIndex = userIndex + 3;
+      }
+    }
+
+    return nextIndex;
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
-    setSteps((prevSteps) => prevSteps + 1);
+    const direction = evt.target.id;
+    const nextIndex = getNextIndex(direction);
+    if (nextIndex !== userIndex) {
+      setUserIndex(nextIndex);
+      setSteps((prevSteps) => prevSteps + 1);
+      setMessage(initialMessage);
+    }
   }
 
   function onChange(evt) {
@@ -64,6 +91,20 @@ export default function AppFunctional(props) {
   function onSubmit(evt) {
     // Use a POST request to send a payload to the server.
     evt.preventDefault();
+    const { x, y } = getXY(userIndex);
+    const apiRequestObj = {
+      x: x,
+      y: y,
+      steps: steps,
+      email: email,
+    };
+
+    axios
+      .post(apiUrl, apiRequestObj)
+      .then((res) => {
+        setMessage(res.data.message);
+      })
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -73,7 +114,7 @@ export default function AppFunctional(props) {
         <h3 id="steps">{`You moved ${steps} times`}</h3>
       </div>
       <div id="grid">
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+        {bArr.map((idx) => (
           <div
             key={idx}
             className={`square${idx === userIndex ? " active" : ""}`}
@@ -83,7 +124,7 @@ export default function AppFunctional(props) {
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button onClick={move} id="left">
