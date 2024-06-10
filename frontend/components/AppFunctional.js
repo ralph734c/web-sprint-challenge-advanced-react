beb20cf446f12b9c9d76fd29eab633e80c7cb043
Christopher Ralph <chris.734c@gmail.com>
2024-06-09 21:47:31 -0400

    Added the correct messages for going too far left, right, up, and down. Minor fix to the "s" in the steps counter.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -4,15 +4,12 @@ import { useState } from "react";
 
 const apiUrl = "http://localhost:9000/api/result";
 
-// Suggested initial states
 const initialMessage = "";
 const initialEmail = "";
 const initialSteps = 0;
 const initialIndex = 4; // the index the "B" is at
 
 export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
   const bArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   const [message, setMessage] = useState(initialMessage);
   const [email, setEmail] = useState(initialEmail);
@@ -20,23 +17,17 @@ export default function AppFunctional(props) {
   const [userIndex, setUserIndex] = useState(initialIndex);
 
   function getXY(idx) {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
     const x = (idx % 3) + 1;
     const y = Math.floor(idx / 3) + 1;
     return { x, y };
   }
 
   function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
     const { x, y } = getXY(userIndex);
     return `Coordinates (${x}, ${y})`;
   }
 
   function reset() {
-    // Use this helper to reset all states to their initial values.
     setMessage(initialMessage);
     setEmail(initialEmail);
     setSteps(initialSteps);
@@ -44,35 +35,30 @@ export default function AppFunctional(props) {
   }
 
   function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
     let nextIndex = userIndex;
 
     if (direction === "left") {
       if (userIndex % 3 !== 0) {
         nextIndex = userIndex - 1;
-      }
+      } setMessage("You can't go left")
     } else if (direction === "up") {
       if (userIndex >= 3) {
         nextIndex = userIndex - 3;
-      }
+      } setMessage("You can't go up")
     } else if (direction === "right") {
       if ((userIndex + 1) % 3 !== 0) {
         nextIndex = userIndex + 1;
-      }
+      } setMessage("You can't go right")
     } else if (direction === "down") {
       if (userIndex < 6) {
         nextIndex = userIndex + 3;
-      }
+      } setMessage("You can't go down")
     }
 
     return nextIndex;
   }
 
   function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
     const direction = evt.target.id;
     const nextIndex = getNextIndex(direction);
     if (nextIndex !== userIndex) {
@@ -83,13 +69,11 @@ export default function AppFunctional(props) {
   }
 
   function onChange(evt) {
-    // You will need this to update the value of the input.
     const { value } = evt.target;
     setEmail(value);
   }
 
   function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
     evt.preventDefault();
     const { x, y } = getXY(userIndex);
     const apiRequestObj = {
@@ -111,7 +95,7 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{getXYMessage(userIndex)}</h3>
-        <h3 id="steps">{`You moved ${steps} times`}</h3>
+        <h3 id="steps">{`You moved ${steps} time${steps === 1 ? "" : "s"}`}</h3>
       </div>
       <div id="grid">
         {bArr.map((idx) => (
